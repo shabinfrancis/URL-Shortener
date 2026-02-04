893d87c77702c23c3e55b3bf478898ea3a5d2184
Shabin Francis <shabinfrancis25@gmail.com>
2026-02-04 19:26:03 +0530

    Typo fix

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { body } from "express-validator"
-import { register, login } from "../controllers/auth.controller.js"
+import { register_user, login_user } from "../controllers/auth.controller.js"
 import wrapAsync from "../utils/tryCatchWrapper.js"
 
 const Router = express.Router()
@@ -10,13 +10,13 @@ Router.post("/register", [
     body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
     body("username").notEmpty().withMessage("Username is required")
 ], 
-    register
+    register_user
 );
 Router.post("/login", [
     body("email").isEmail().withMessage("Invalid email address"),
     body("password").notEmpty().withMessage("Password is required")
 ], 
-    login
+    login_user
 );
 
 export default Router;
