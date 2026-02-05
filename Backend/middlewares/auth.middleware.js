49e99bdba4b1e9cfe64368eb1433ef85bbe248ad
Shabin Francis <shabinfrancis25@gmail.com>
2026-02-05 21:54:42 +0530

    Implement authentication middleware for JWT token verification

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,16 @@
+import { verifyToken } from "../utils/helper.js"
+import { findUserById } from "../dao/user.dao.js"
+
+export const authMiddleware = async (requestAnimationFrame, res, next) => {
+    const token = req.cookies.accessToken
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    try {
+        const decoded = verifyToken(token);
+        const user = await findUserById(decoded);
+        if (!user) return res.status(401).json({ message: "Unauthorized" });
+        req.user = user
+        next()
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorized", error })
+    }
+}
